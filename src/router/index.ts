31d935619feb9dfb588a20bfc645662bc8225987
Doru <doru.dev.now@gmail.com>
2020-03-12 18:01:20 +0100

    adapt previous implementation to the new repo

---
+++ src/router/index.ts
@@ -0,0 +1,20 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import LicitatiiView from "@/views/LicitatiiView.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    component: LicitatiiView
+  }
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes
+});
+
+export default router;
